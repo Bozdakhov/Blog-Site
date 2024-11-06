b50b84dab95f4f3b05eabc208018f07288172b97
Bozdakhov <jenisbozdak@gmail.com>
2024-11-06 17:08:10 +0500

    div class email label updated

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -29,11 +29,7 @@
                     <p style="color: red">{{'* ' . $message}}</p>
                 @enderror
             </div>
-            <div class="mb-4">
-                <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
-                <input type="email" id="email" name="email" value="{{old('email')}}" required
-                    class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500">
-            </div>
+            //
             <div>
                 @error('email')
                     <p style="color: red">{{'* ' . $message}}</p>
